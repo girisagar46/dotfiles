4a4216ad2154c020f3c5fa5cdf3d2915e3197313
sagar.giri <sagar.giri@bebit.com>
2021-03-16 16:52:23 +0900

    Add custom dotfiles.

--- brew.sh
+++ brew.sh
@@ -52,37 +52,12 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
 # Install other useful binaries.
 brew install ack
 #brew install exiv2
 brew install git
 brew install git-lfs
+brew install git-flow-avh
 brew install gs
 brew install imagemagick --with-webp
 brew install lua
@@ -95,7 +70,12 @@ brew install rlwrap
 brew install ssh-copy-id
 brew install tree
 brew install vbindiff
-brew install zopfli
+brew install ccat
+
+# zsh
+brew install zsh-syntax-highlighting
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+
